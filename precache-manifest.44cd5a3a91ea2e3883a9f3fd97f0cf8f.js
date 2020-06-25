2ffadddcf3fb6aa363163bf3dc50c5d1babf03db
Justin Sandahl <sandahl.j.d@gmail.com>
2020-06-25 18:51:48 +0100

    Updates

---
+++ precache-manifest.44cd5a3a91ea2e3883a9f3fd97f0cf8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbc9d5539d5cd88c44b4c3c6041e35b",
+    "url": "/react-NASA/index.html"
+  },
+  {
+    "revision": "bddd7148b787f35d1e38",
+    "url": "/react-NASA/static/css/main.ead70764.chunk.css"
+  },
+  {
+    "revision": "f401b2dbaa72a8b3b29a",
+    "url": "/react-NASA/static/js/2.ea3fad5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-NASA/static/js/2.ea3fad5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bddd7148b787f35d1e38",
+    "url": "/react-NASA/static/js/main.fca37885.chunk.js"
+  },
+  {
+    "revision": "ebefbb6a74f7e89cea21",
+    "url": "/react-NASA/static/js/runtime-main.b6c89495.js"
+  }
+]);
